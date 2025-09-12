b418b5edcb0bfe22a26776d755400abbb1b4fd14
User <User@T29>
2025-09-12 08:22:03 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TraficLight
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    
+    }
+
+}
